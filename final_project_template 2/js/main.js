14ebe3743498fbefb926fc2f58cce3a276bbd2f4
rachelciancio <32886077+rachelciancio@users.noreply.github.com>
2017-12-19 19:18:20 -0800

    Add files via upload
    FEWD remote final project submission 12/19

--- final_project_template 2/js/main.js
+++ final_project_template 2/js/main.js
@@ -1,1 +1,21 @@
 // Make it rain!
+
+$('section h2').on('click', function() {
+  console.log($(this).siblings('.panel'));
+
+  $(this).next(".panel").toggle("slow");
+} );
+
+$('faq-grid h3').on('click', function() {
+  console.log($(this).siblings('.dropdown'));
+
+  $(this).next(".dropdown").toggle("slow");
+} );
+
+// insert photo carousel for about section
+
+// insert jumplink to contact section
+
+// insert jumplink from schedule appt button to appointment request form
+
+// insert jumplink from about/groups/HERE to contact section
